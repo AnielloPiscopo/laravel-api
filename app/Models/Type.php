fa3ca852240361c19a837a778edb7f87b9bdf400
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-02 15:45:49 +0100

    first commit

---
+++ app/Models/Type.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    protected $fillable = array('name' , 'slug' , 'vanilla');
+
+    public function projects(){
+        return $this->hasMany(Project::class);
+    }
+    
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
