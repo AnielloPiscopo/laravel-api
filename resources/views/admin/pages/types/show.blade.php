9f7c41a8cd90149dcb23a2502df5d87afb815934
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-06 00:12:58 +0100

    Modified the show of the types folder

--- resources/views/admin/pages/types/show.blade.php
+++ resources/views/admin/pages/types/show.blade.php
@@ -13,21 +13,32 @@
 @section('title' , config('app.name', 'Laravel') . '- Types')
 
 @section('content')
-@php
-    $listElements = [
-        'name',
-    ]
-@endphp
 
-<article class="my_card">
-    <ul>
-        @foreach ($listElements as $listEl)
-            <li>{{$listEl . ':' . $type->$listEl}}</li> 
-        @endforeach
-    </ul>
-
-    <div class="my_btn-container d-flex justify-content-center">
-        <a href="{{route('admin.pages.types.edit' , $type->slug)}}" class="my_btn btn btn-dark">Edit</a>
-    </div>
-</article>
+<section>
+    @foreach ($type->projects as $project)
+    <article class="card text-center">
+        <div class="card-header">
+                <span class="badge" style="color:{{$type->color}};background-color:{{$type->bg_color}}">{{$type->name}}</span>
+                @foreach ($project->technologies as $technology)
+                    <span class="badge" style="color:{{$technology->color}};background-color:{{$technology->bg_color}}">{{$technology->name}}</span>
+                @endforeach
+            </div>
+        </div>
+        <div class="card-image my-4">
+            @if ( $project->isImageAUrl())
+            <img src="{{ $project->img_path }}"
+            @else
+            <img src="{{ asset("storage/$project->img_path") }}"
+            @endif
+            alt="{{ $project->title }} image" class="img-fluid">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ $project->title }}</h5>
+            <p class="card-text">
+                {{$project->description}}
+            </p>
+        </div>
+    </article>
+    @endforeach
+</section>
 @endsection
