878c191fcf874b1f6f6e0b1a2007e86975c68aad
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-11 17:59:40 +0100

    Corrected the name of the classes linked with the recent created tables in their specific models

--- app/Models/Programm.php
+++ app/Models/Programm.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Type extends BaseModel
+class Programm extends BaseModel
 {
     use HasFactory;
 
